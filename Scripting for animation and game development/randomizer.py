81eb38b92e4885bd6f47b05909ae50181c49081f
Brandon Riley <github@briley.me>
2023-11-12 17:16:08 -0700

    randomizer update

--- Scripting for animation and game development/randomizer.py
+++ Scripting for animation and game development/randomizer.py
@@ -2,17 +2,8 @@ import maya.cmds as cmds
 import random
 
 def duplicate_and_disperse(num_duplicates, minX, maxX, minY, maxY, minZ, maxZ):
-    """
-    Duplicates selected objects and randomly disperses them within given ranges.
 
-    Args:
-    num_duplicates (int): Number of times to duplicate the selected objects.
-    minX, maxX (float): Minimum and maximum values for the X-axis.
-    minY, maxY (float): Minimum and maximum values for the Y-axis.
-    minZ, maxZ (float): Minimum and maximum values for the Z-axis.
-    """
-
-    # Ensure there are objects selected
+    # make sure there are objects selected
     selected_objects = cmds.ls(selection=True)
     if not selected_objects:
         print("No objects selected. Please select objects to duplicate.")
