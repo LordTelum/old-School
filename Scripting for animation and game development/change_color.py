f0b8093bc06c087a55a52da2637a4889355e02b5
LordTelum <122577823+LordTelum@users.noreply.github.com>
2023-12-14 16:49:47 -0700

    Added UI to change_color.py

--- Scripting for animation and game development/change_color.py
+++ Scripting for animation and game development/change_color.py
@@ -23,5 +23,33 @@ def change_color(color_index):
             cmds.setAttr(f"{shape}.overrideEnabled", 1)
             cmds.setAttr(f"{shape}.overrideColor", color_index)
 
-# Example usage
-change_color(6)
+def create_color_changer_ui():
+    # Check if the window exists
+    if cmds.window("colorChangerUI", exists=True):
+        cmds.deleteUI("colorChangerUI", window=True)
+
+    # Create the window
+    cmds.window("colorChangerUI", title="Color Changer UI", widthHeight=(400, 120))
+
+    # Create a layout
+    cmds.columnLayout(adjustableColumn=True)
+
+    # UI elements
+    color_index_slider = cmds.intSliderGrp(field=True, label="Color Index:",
+                                           minValue=0, maxValue=31, value=0,
+                                           fieldMinValue=0, fieldMaxValue=31,
+                                           columnWidth=[(1, 100), (2, 50), (3, 250)])  # Adjust column widths
+
+    # Function to handle slider change
+    def on_slider_change(*args):
+        slider_value = cmds.intSliderGrp(color_index_slider, query=True, value=True)
+        change_color(slider_value)
+
+    # Button to execute the function
+    cmds.button(label="Zhu Li do the thing!", command=on_slider_change)
+
+    # Show the window
+    cmds.showWindow("colorChangerUI")
+
+# Call the function to create the UI
+create_color_changer_ui()
